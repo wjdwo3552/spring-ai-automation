f19601f36a82b11a7bacd86e6286155d7f8f98e3
wjdwo <a01093019627@gmail.com>
2026-01-05 17:00:19 +0900

    Initial commit - Spring AI Automation Server

---
+++ src/main/java/com/example/automation/AutomationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/automation/AutomationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
